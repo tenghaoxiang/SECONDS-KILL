a243bba5231f8cfb7997972ce44ea30fc970f8c4
tenghaoxiang <873030913@qq.com>
2019-06-21 22:05:56 +0800

    first commit

---
+++ src/main/java/top/haibaraai/secondskill/SecondsKillApplication.java
@@ -0,0 +1,13 @@
+package top.haibaraai.secondskill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondsKillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondsKillApplication.class, args);
+    }
+
+}
