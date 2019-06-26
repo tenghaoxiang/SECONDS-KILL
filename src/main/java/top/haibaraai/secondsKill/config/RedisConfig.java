10adc2ffb0c57a73b4b8fecdab60544a6df1534c
tenghaoxiang <873030913@qq.com>
2019-06-26 13:33:19 +0800

    修改redisTemplate中键的序列化方式

--- src/main/java/top/haibaraai/secondsKill/config/RedisConfig.java
+++ src/main/java/top/haibaraai/secondsKill/config/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -11,6 +12,7 @@ public class RedisConfig {
     @Bean
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(RedisSerializer.string());
         redisTemplate.setConnectionFactory(factory);
         return redisTemplate;
     }
